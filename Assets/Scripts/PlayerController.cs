2e96e263fca064583ff4d75d6a29fce17b2b553d
JaamoK <jarno.kankainen1@gmail.com>
2026-03-03 13:48:14 +0200

    Working movement with jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D PlayerRigbod2D;
+    public float force = 0f;
+    public float jumpForce = 0f;
+    Vector2 movement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PlayerRigbod2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        movement = new Vector2(horizontal, 0).normalized;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("JUMP");
+            PlayerRigbod2D.AddForce(transform.up * jumpForce,ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(movement * force * Time.deltaTime,Space.World);
+    }
+}
