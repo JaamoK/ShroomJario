5987979f7f49e0f626e4cc22e25566b1c6047d6d
JaamoK <jarno.kankainen1@gmail.com>
2026-03-04 10:29:58 +0200

    Jump sounds added. Movement fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,6 +8,11 @@ public class PlayerController : MonoBehaviour
     public float force = 0f;
     public float jumpForce = 0f;
     Vector2 movement;
+    bool grounded;
+
+    public AudioResource hyppySound;
+    public AudioResource hitSound;
+    public AudioResource deathSound;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,15 +26,42 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         movement = new Vector2(horizontal, 0).normalized;
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             Debug.Log("JUMP");
-            PlayerRigbod2D.AddForce(transform.up * jumpForce,ForceMode2D.Impulse);
+            PlayerRigbod2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            grounded = false;
         }
     }
 
     private void FixedUpdate()
     {
-        transform.Translate(movement * force * Time.deltaTime,Space.World);
+        transform.Translate(movement * force * Time.deltaTime);
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Common Style
+        if (collision.transform.tag == "Ground")
+        {
+            Debug.Log("IsGrounded");
+            grounded = true;
+        }
+
+        //Hardcore Style
+        //if (collision.transform.parent.name == "Platforms")
+        //{
+        //    grounded = true;
+        //}
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            Debug.Log("NotGrounded");
+            grounded = false;
+        }
+    }
+
 }
